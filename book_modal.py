3686a08ab0801c07fbde790b4d0103ec91278e98
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-13 10:58:35 +0500

    returns book modal content

---
+++ book_modal.py
@@ -0,0 +1,23 @@
+import flet as ft
+
+class BookModal(ft.Column):
+    def __init__(self, isbn, title, author):
+        super().__init__()
+        self.modal_data = {
+            'isbn': isbn,
+            'title': title,
+            'author': author
+        }
+
+    def build(self):
+        description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet urna eu erat aliquam gravida vitae eu ligula. Nulla sed congue lectus. Mauris sit amet lacinia est, ac imperdiet magna. Maecenas vel eros suscipit orci bibendum egestas. Vivamus lacinia dui vel lacus finibus placerat. Integer aliquet ligula quis est condimentum pellentesque. Pellentesque ante metus, pulvinar vel ultricies sit amet, suscipit vitae ligula. Fusce vestibulum at velit et lacinia. Donec maximus orci eget nibh sagittis pharetra. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras libero justo, ultrices vel ipsum ac, laoreet luctus lorem. Quisque placerat molestie est, a venenatis neque fringilla nec.'
+        content = ft.Row([
+                    ft.Text(description),
+                    ft.Image(
+                        src='https://www.designforwriters.com/wp-content/uploads/2017/10/design-for-writers-book-cover-tf-2-a-million-to-one.jpg',
+                        width=300,
+                        fit=ft.ImageFit.CONTAIN
+                    )
+                ])
+
+        return content
