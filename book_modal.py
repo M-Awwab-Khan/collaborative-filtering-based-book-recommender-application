647e1f1b79efb2626e43a7d679ac6e58ade9bdc6
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-13 22:27:09 +0500

    fixed recommendations placement

--- book_modal.py
+++ book_modal.py
@@ -29,11 +29,11 @@ class BookModal(ft.UserControl):
                             ft.Text(f"ISBN 13: ", spans=[ft.TextSpan(f"{data['industryIdentifiers'][1]['identifier']}", ft.TextStyle(weight=ft.FontWeight.NORMAL))], weight=ft.FontWeight.BOLD, size=17),
                             ft.Text(f"Ratings: ", spans=[ft.TextSpan(f"{self.modal_data['num_ratings']} ({self.modal_data['avg_rating']})", ft.TextStyle(weight=ft.FontWeight.NORMAL))], weight=ft.FontWeight.BOLD, size=17),
                         ], spacing=17),
-                        ft.Row(
+                        
+                    ], vertical_alignment=ft.CrossAxisAlignment.START, alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
+                    ft.Row(
                             [BookPreview(**self.modal_data)]
                         )
-                    ], vertical_alignment=ft.CrossAxisAlignment.START, alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
-
                 ], height=700, scroll=ft.ScrollMode.ADAPTIVE)
 
         return ft.Container(content=content, padding=20)
