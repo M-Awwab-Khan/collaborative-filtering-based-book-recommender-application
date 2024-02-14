e6feb7a9b9159552e7986d6a479abf8e9baf30f7
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-14 13:23:20 +0500

    extracted search term and printed suggestions from model

--- search_page.py
+++ search_page.py
@@ -1,22 +1,30 @@
 import flet as ft
+from search import search
 
 class SearchPage(ft.UserControl):
     def build(self):
         self.cards_grid = ft.Row(wrap=True)
-        search_field = ft.TextField(label="Search Book", hint_text="Enter book name, author name, publisher or isbn", width=900, border_width=0.7, border_radius=10)
+        self.search_field = ft.TextField(label="Search Book", hint_text="Enter book name, author name, publisher or isbn", width=900, border_width=0.7, border_radius=10)
         return ft.Container(
             content=ft.Column([
                 ft.Text('Search Books', theme_style=ft.TextThemeStyle.DISPLAY_MEDIUM, weight=ft.FontWeight.BOLD),
                 ft.Row([
-                    search_field,
+                    self.search_field,
                     ft.FloatingActionButton(
                         icon=ft.icons.SEARCH_ROUNDED,
                         tooltip="Search",
-                        shape=ft.CircleBorder()
+                        shape=ft.CircleBorder(),
+                        on_click=self.show_suggestions
                     )
                 ]),
                 self.cards_grid
             ], spacing=30),
             padding=50
         )
-    
+    def show_suggestions(self, e):
+        search_term = self.search_field.value
+        suggestions = search(search_term)
+
+        if suggestions.shape[0] > 0:
+            for _, row in suggestions.iterrows():
+                print(row)
