848f36b328dbf04d5dfad6cd3629279962e966a2
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-14 13:13:16 +0500

    designed search page

--- search_page.py
+++ search_page.py
@@ -2,4 +2,21 @@ import flet as ft
 
 class SearchPage(ft.UserControl):
     def build(self):
-        return ft.Text('You are on search page', theme_style=ft.TextThemeStyle.DISPLAY_LARGE)
+        self.cards_grid = ft.Row(wrap=True)
+        search_field = ft.TextField(label="Search Book", hint_text="Enter book name, author name, publisher or isbn", width=900, border_width=0.7, border_radius=10)
+        return ft.Container(
+            content=ft.Column([
+                ft.Text('Search Books', theme_style=ft.TextThemeStyle.DISPLAY_MEDIUM, weight=ft.FontWeight.BOLD),
+                ft.Row([
+                    search_field,
+                    ft.FloatingActionButton(
+                        icon=ft.icons.SEARCH_ROUNDED,
+                        tooltip="Search",
+                        shape=ft.CircleBorder()
+                    )
+                ]),
+                self.cards_grid
+            ], spacing=30),
+            padding=50
+        )
+    
