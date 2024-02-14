4f4122b1116d79bf5b215b3e07c9aaa7b904078f
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-14 17:23:47 +0500

    feedback for no search results

--- search_page.py
+++ search_page.py
@@ -7,7 +7,7 @@ modal_manager = ModalManager()
 
 class SearchPage(ft.UserControl):
     def build(self):
-        self.cards_grid = ft.Row(wrap=True)
+        self.cards_grid = ft.Row(wrap=True, alignment=ft.MainAxisAlignment.CENTER, width=1000)
         self.search_field = ft.TextField(label="Search Book", hint_text="Enter book name, author name, publisher or isbn", width=900, border_width=0.7, border_radius=10)
         return ft.Container(
             content=ft.Column([
@@ -30,9 +30,13 @@ class SearchPage(ft.UserControl):
         suggestions = search(search_term)
 
         if suggestions.shape[0] > 0:
+            self.cards_grid.controls.clear()
             for _, row in suggestions.iterrows():
                 to_insert = BookCard(row['ISBN'], row['Book-Title'], row['Book-Author'], row['Year-Of-Publication'], row['num_ratings'], row['avg_rating'], open_modal=modal_manager.open_book_modal)
                 self.cards_grid.controls.append(to_insert)
                 self.update()
+        else:
+            self.cards_grid.controls.append(ft.Text("Sorry! Your search yielded no results.", size=25, theme_style=ft.TextTheme.title_large))
+            self.update()
 
     
