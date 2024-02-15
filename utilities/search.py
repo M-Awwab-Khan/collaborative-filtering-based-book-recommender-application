96795b3ddfaed5911c936874411250c3697c2c3a
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-15 09:46:54 +0500

    bug fixed

--- utilities/search.py
+++ utilities/search.py
@@ -1,6 +1,6 @@
 import pickle
 import os
-final_ratings_app = pickle.load(open(os.path.join(os.getcwd(), 'data', 'similarity_score.pkl'), 'rb'))
+final_ratings_app = pickle.load(open(os.path.join(os.getcwd(), 'data', 'final_ratings_app.pkl'), 'rb'))
 
 def search(search_term):
   mask = final_ratings_app.apply(lambda col: col.astype(str).str.contains(search_term, case=False, regex=True)).any(axis=1)
